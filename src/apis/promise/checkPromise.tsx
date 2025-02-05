84ec51eef69445614ee953b42779e4d208f2867c
junghyungo <pp90971658@gmail.com>
2025-02-05 11:31:42 +0900

    탐색&약속 페이지 상단패딩 제거, 약속 api 추가

---
+++ src/apis/promise/checkPromise.tsx
@@ -0,0 +1,23 @@
+import instance from "../instance/index";
+
+const CheckPromise ={
+  checkPromises: async () => {
+    try {
+      const response = await instance.get("/users/promises", {
+        headers: { "Accept": "application/json" }
+      });
+      return response;
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 404) {
+          alert("사용자의 약속 정보를 찾을 수 없습니다.");
+        } else {
+          alert("잠시 후 다시 시도해주세요.");
+        }
+      }
+      throw error;
+    }
+  }
+};
+
+export default CheckPromise;
