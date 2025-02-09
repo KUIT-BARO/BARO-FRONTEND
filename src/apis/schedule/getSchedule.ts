b5a035fa984d735bff5f4cc6aeff464d773f5b9e
Jihyeon Lee <ljh130334@naver.com>
2025-02-09 16:49:15 +0900

    feat: 자신의 시간표 조회 API 연결 완료

---
+++ src/apis/schedule/getSchedule.ts
@@ -0,0 +1,15 @@
+import instance from "../instance";
+
+export const getSchedule = {
+  getMySchedule: async () => {
+    try {
+      const response = await instance.get("/users/my-schedule");
+      return response.data;
+    } catch (error) {
+      if (error.response?.status === 400) {
+        alert("시간표 조회에 실패했습니다.");
+      }
+      throw error;
+    }
+  }
+};
