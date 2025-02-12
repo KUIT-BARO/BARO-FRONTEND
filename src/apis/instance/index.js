b5dc8e4e2d5770a16d2411147b8d38f67df6a54e
Jihyeon Lee <ljh130334@naver.com>
2025-02-12 17:50:07 +0900

    feat: index.js, vite.config.js 수정

--- src/apis/instance/index.js
+++ src/apis/instance/index.js
@@ -1,16 +1,32 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//   // baseURL: process.env.NODE_ENV === 'production'
+//   baseURL: import.meta.env.NODE_ENV === 'production'
+//     ? "http://3.38.47.233"
+//     : "http://localhost:8080",
+//   timeout: 10000,
+//   withCredentials: true,  // 쿠키 포함 설정
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Access-Control-Allow-Origin": "*",
+//   }
+// });
+
+// export default instance;
+
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: process.env.NODE_ENV === 'production' 
-  baseURL: import.meta.env.NODE_ENV === 'production'
-    ? "http://3.38.47.233" 
-    : "http://localhost:8080",
+  baseURL:
+    import.meta.env.NODE_ENV === "production"
+      ? "http://3.38.47.233" // 배포 서버 URL
+      : "/api", // Vite 프록시 사용
   timeout: 10000,
-  withCredentials: true,  // 쿠키 포함 설정
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-  }
+  },
 });
 
 export default instance;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: "http://jsonplaceholder.typicode.com",
+      "/api": {
+        target: "http://localhost:8080",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""), // "/api" 제거
       },
     },
-  }
-})
+  },
+});
