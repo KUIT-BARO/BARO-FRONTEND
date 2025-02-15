e48d8e039a02c04f86bdbc104113e9e1dfdbc4f4
hamxxn <hamin031121@gmail.com>
2025-02-15 23:26:33 +0900

    로그인 이후 모든 페이지 접근 가능하게 수정

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,7 +1,12 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ isAuthenticated, children }) => {
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
+  if (!isAuthenticated) {
+    console.log("인증되지 않음, 로그인 페이지로 이동");
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
