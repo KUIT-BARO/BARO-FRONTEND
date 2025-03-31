1ef83b8a3597c3474feec69b4edfe7a68e78354b
Hwanggyuun <gka365@naver.com>
2025-03-31 13:07:54 +0900

    interface 설정정

---
+++ src/interface/api/schedules/schedule.ts
@@ -0,0 +1,29 @@
+export interface ResponseSchedule {
+  scheduleId: number;
+  scheduleName: string;
+  dayOfWeek: number;
+  startTime: string;
+  endTime: string;
+}
+
+export interface UserData {
+  profileImage: string;
+  userName: string;
+  email: string;
+  schedules: ResponseSchedule[];
+}
+
+export interface UserResponse {
+  success: boolean;
+  code: number;
+  message: string;
+  data: UserData;
+}
+
+export interface RequestSchedule {
+  scheduleName: string;
+  dayOfWeek: number;
+  startTime: string;
+  endTime: string;
+  placeName?: string;
+}
