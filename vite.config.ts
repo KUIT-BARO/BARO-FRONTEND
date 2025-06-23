9b964c0d0532a085dc393018eba3b607d4d5ac54
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-06-23 15:57:58 +0900

    init: identifiers 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin({ identifiers: 'debug' })],
 });
