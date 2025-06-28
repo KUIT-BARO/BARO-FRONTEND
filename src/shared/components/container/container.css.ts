76e136967fd26f42916121903d2d00ee8b9960bb
hwanggyuun <gka365@naver.com>
2025-06-28 19:00:24 +0900

    init-Layout component

---
+++ src/shared/components/container/container.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  padding: '0 1.25rem', //20px
+  maxWidth: '27rem', // 432px 정도 (모바일 기준)
+  margin: '0 auto',
+  width: '100%',
+});
