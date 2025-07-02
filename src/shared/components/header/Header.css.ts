368ffde545188022f83f41b181e66123b7208e0d
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-02 14:04:22 +0900

    feat: header 공통 컴포넌트 생성

---
+++ src/shared/components/header/Header.css.ts
@@ -0,0 +1,49 @@
+import { vars } from '@shared/styles/theme.css';
+import { recipe } from '@vanilla-extract/recipes';
+
+export const headerWrapper = recipe({
+  base: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+
+    width: '100%',
+    height: '9.8rem',
+    border: '1px solid black',
+  },
+  variants: {
+    background: {
+      blue0: {
+        background: vars.color.blue0,
+      },
+      baroblue: {
+        background: vars.color.baroBlue,
+      },
+    },
+  },
+});
+
+export const iconWrapper = recipe({
+  base: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '1.1rem',
+    cursor: 'pointer',
+  },
+  variants: {
+    type: {
+      icon: {
+        width: '4.4rem',
+        height: '4.4rem',
+      },
+      logo: {
+        width: '6rem',
+        height: '5rem',
+      },
+    },
+  },
+  defaultVariants: {
+    type: 'icon',
+  },
+});
