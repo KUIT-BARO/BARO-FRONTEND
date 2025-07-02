f7d4c1fa8cf831e8be188c647c6b730fd8d1982c
junghyungo <pp90971658@gmail.com>
2025-07-02 12:56:04 +0900

    [code review] TS 설정 및 정적파일 디렉토리 등록

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,28 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
     "@storybook/addon-themes",
   ],
-  "framework": {
+  framework: {
     "name": "@storybook/react-vite",
     "options": {}
-  }
+  },
+  typescript: {
+    // 타입 체크 활성화
+    check: true,
+    // props 자동 추출 도구
+    reactDocgen: "react-docgen-typescript"
+  },
+  // 정적 리소스 디렉토리 (이미지, 폰트 등)
+  staticDirs: ['../public'],
 };
 export default config;
