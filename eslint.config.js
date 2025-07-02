4079c3b56ee0ceebdc21588bba004761c39a8f4e
junghyungo <pp90971658@gmail.com>
2025-07-02 12:49:04 +0900

    [code review] 주석 수정

--- eslint.config.js
+++ eslint.config.js
@@ -33,18 +33,24 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'no-duplicate-imports': 'error', //중복 Import 안돼용
-      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }], //콘솔은 확인 뒤 지웁시당
+      //중복 Import 안돼용
+      'no-duplicate-imports': 'error',
+      //콘솔은 확인 뒤 지웁시당
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
       'no-unused-vars': [
         'error',
         {
           argsIgnorePattern: '^_',
         },
       ],
-      'no-multiple-empty-lines': 'error', //공백 금지
-      'no-undef': 'error', //정의 안 한 변수 사용 x
-      indent: ['error', 2], //들여쓰기 2칸!
-      'no-trailing-spaces': 'error', // 쓸데없는 공백 없애기
+      //공백 금지
+      'no-multiple-empty-lines': 'error',
+      //정의 안 한 변수 사용 x
+      'no-undef': 'error',
+      //들여쓰기 2칸!
+      indent: ['error', 2],
+      // 쓸데없는 공백 없애기
+      'no-trailing-spaces': 'error',
       'import/newline-after-import': ['error', { count: 1 }],
     },
   },
