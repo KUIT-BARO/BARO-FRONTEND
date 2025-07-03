d2b598f94d931411c14dea70b56b26f7d2ae32a7
hwanggyuun <gka365@naver.com>
2025-07-03 14:20:23 +0900

    [feat] 약속 제안 공통 컴포넌트 css 설정

---
+++ src/shared/components/appointmentmanagementitem/AppointmentManagementItem.css.ts
@@ -0,0 +1,27 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '@shared/styles/theme.css';
+
+export const container = style({
+  display: 'flex',
+  width: '15rem',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '0.8rem',
+});
+
+export const itembox = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '1.2rem',
+  alignSelf: 'stretch',
+});
+
+export const textStyle = style({
+  color: vars.color.gray3,
+  fontFamily: 'Pretendard',
+  fontFeatureSettings: "'liga' off, 'clig' off",
+  fontSize: vars.font.body_thin_14.fontSize,
+  fontWeight: vars.font.body_thin_14.fontWeight,
+  lineHeight: vars.font.body_thin_14.lineHeight,
+  letterSpacing: '-0.35px',
+});
