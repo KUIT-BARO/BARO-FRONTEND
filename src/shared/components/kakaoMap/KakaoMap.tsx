8bbc3eebb0a21374664fd0bf1b647bbfbadf06d5
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-04 16:30:24 +0900

    feat: kakaomap 스타일 정의

--- src/shared/components/kakaoMap/KakaoMap.tsx
+++ src/shared/components/kakaoMap/KakaoMap.tsx
@@ -1,16 +1,27 @@
+import { useMemo } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import useKakaoLoader from '@shared/components/kakaoMap/hooks/useKakaoLoader';
 import useGetLocation from '@shared/components/kakaoMap/hooks/useGetLocation';
+import type { LatLng } from '@shared/components/kakaoMap/types/latLng';
+import { MAP_SIZE, type MapSize } from '@shared/components/kakaoMap/constant/mapSize';
+import * as styles from '@shared/components/kakaoMap/KakaoMap.css';
 
-const KakaoMap = () => {
+interface KakaoMapProps {
+  center: LatLng | null;
+  size?: MapSize;
+}
+
+const KakaoMap = ({ center, size = MAP_SIZE.MEDIUM }: KakaoMapProps) => {
   const loaded = useKakaoLoader();
-  const { center, position } = useGetLocation();
+  const { position } = useGetLocation();
+
+  const fallbackCenter = useMemo(() => center ?? position, [center, position]);
 
   if (!loaded) return <div>지도 불러오는 중...</div>;
 
   return (
-    <Map center={center} style={{ width: '100%', height: '360px' }}>
-      <MapMarker position={position} />
+    <Map center={fallbackCenter} className={styles.mapWrapper({ size })}>
+      <MapMarker position={fallbackCenter} />
     </Map>
   );
 };
