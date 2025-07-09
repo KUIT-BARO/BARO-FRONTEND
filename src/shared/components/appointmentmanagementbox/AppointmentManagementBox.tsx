8fbee22b820b96dcd2b922dd131ff21367dce0ea
hwanggyuun <gka365@naver.com>
2025-07-09 17:45:45 +0900

    [style] css 고정 좌표에서 center로 변경

--- src/shared/components/appointmentmanagementbox/AppointmentManagementBox.tsx
+++ src/shared/components/appointmentmanagementbox/AppointmentManagementBox.tsx
@@ -2,32 +2,8 @@ import type { AppointmentManagementItemProps } from '@shared/components/appointm
 import * as styles from '@shared/components/appointmentmanagementbox/AppointmentManagementBox.css';
 import AppointmentManagementItem from '@shared/components/appointmentmanagementitem/AppointmentManagementItem';
 import { IcArrow } from '@svg/index';
-import React from 'react';
 import Text from '@shared/components/text/Text';
 
-type StatusType = 'suggestedPromises' | 'votingPromises' | 'confirmedPromises';
-interface MinibuttonProps {
-  status: StatusType;
-}
-
-const Minibutton: React.FC<MinibuttonProps> = ({ status }) => {
-  const title =
-    status === 'suggestedPromises'
-      ? '미정'
-      : status === 'votingPromises'
-        ? '투표'
-        : status === 'confirmedPromises'
-          ? '확정'
-          : '?';
-
-  return (
-    <div className={styles.minibutton({ background: status })}>
-      <Text tag="body_bold_14" color="white">
-        {title}
-      </Text>
-    </div>
-  );
-};
 
 function AppointmentManagementBox(props: AppointmentManagementItemProps) {
   return (
@@ -38,7 +14,6 @@ function AppointmentManagementBox(props: AppointmentManagementItemProps) {
       </div>
       <div className={styles.bottomBox}>
         <AppointmentManagementItem {...props} />
-        <Minibutton status={props.status} />
       </div>
     </div>
   )
--- src/shared/components/appointmentmanagementitem/AppointmentManagementItem.css.ts
+++ src/shared/components/appointmentmanagementitem/AppointmentManagementItem.css.ts
@@ -1,4 +1,6 @@
 import { style } from '@vanilla-extract/css';
+import { vars } from '@shared/styles/theme.css';
+import { recipe } from '@vanilla-extract/recipes';
 
 export const container = style({
   display: 'flex',
@@ -25,4 +27,33 @@ export const iconStyle = style({
   width: '1.8rem',
   height: '1.8rem',
   flexShrink: 0,
+});
+export const thirdStyle = style({
+  width: '32rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+});
+
+export const minibutton = recipe({
+  base: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '1rem',
+
+    width: '4.6rem',
+    height: '2.4rem',
+    padding: '0.2rem 1.1rem',
+    flexShrink: 0,
+    borderRadius: '10rem',
+  },
+  variants: {
+    background: {
+      suggestedPromises: { background: vars.color.baroBlue },
+      votingPromises: { background: vars.color.yellow1 },
+      confirmedPromises: { background: vars.color.red1 },
+    },
+  },
 });
