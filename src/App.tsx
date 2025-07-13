5e7ee6e3d2d0d7c5b17d9db3b96e99dc1db9ae33
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-13 09:23:19 +0900

    feat: react query devtool 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import router from '@router/Router';
 import '@styles/global.css.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
