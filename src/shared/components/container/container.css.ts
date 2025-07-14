5705523a8635dfb780f88bb015fb463c919163d7
hwanggyuun <gka365@naver.com>
2025-07-14 18:09:17 +0900

    [refactory]rem 단위 수정

--- src/shared/components/container/container.css.ts
+++ src/shared/components/container/container.css.ts
@@ -1,10 +1,8 @@
 import { style } from '@vanilla-extract/css';
 
-//maxWidth 432px 정도를 rem으로 환산하면 27rem (모바일 기준)
-//패딩 20px을 rem으로 환산하면 1.25rem
 export const container = style({
   width: '100%',
-  maxWidth: '27rem',
-  padding: '0 1.25rem',
+  maxWidth: '43.2rem',
+  padding: '0 0.2rem',
   margin: '0 auto',
 });
