6f73cb6ab2a7c7bbd83702bb29c7fc182bc54440
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-25 12:33:49 +0900

    refactor: center 옵셔널로 수정

--- src/shared/components/kakaoMap/hooks/useGetLocation.ts
+++ src/shared/components/kakaoMap/hooks/useGetLocation.ts
@@ -1,23 +1,27 @@
 import { useEffect, useState } from 'react';
 import type { LatLng } from '@shared/components/kakaoMap/types/latLng';
 
-function useGetlocation() {
-  const [center, setCenter] = useState<LatLng>({ lat: 33.450701, lng: 126.570667 });
+function useGetlocation(center?: LatLng) {
+  const [currentCenter, setCurrentCenter] = useState<LatLng>({ lat: 33.450701, lng: 126.570667 });
   const [position, setPosition] = useState<LatLng>({ lat: 33.450701, lng: 126.570667 });
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(pos => {
-      setCenter({ lat: pos.coords.latitude, lng: pos.coords.longitude });
-    });
+    if (center) {
+      setCurrentCenter(center);
+    } else {
+      navigator.geolocation.getCurrentPosition(pos => {
+        setCurrentCenter({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+      });
+    }
 
     const watchId = navigator.geolocation.watchPosition(pos => {
       setPosition({ lat: pos.coords.latitude, lng: pos.coords.longitude });
     });
 
     return () => navigator.geolocation.clearWatch(watchId);
-  }, []);
+  }, [center]);
 
-  return { center, position };
+  return { currentCenter, position };
 }
 
 export default useGetlocation;
