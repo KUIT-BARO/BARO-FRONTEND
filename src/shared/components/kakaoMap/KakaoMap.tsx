0a5f8ee118f36ee76e1b9dbf4b7f4c67da0c85ed
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-25 13:11:22 +0900

    feat: 핀 아이콘이 있을 시, 화면에 아이콘 찍기

--- src/shared/components/kakaoMap/KakaoMap.tsx
+++ src/shared/components/kakaoMap/KakaoMap.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import useKakaoLoader from '@shared/components/kakaoMap/hooks/useKakaoLoader';
 import useGetLocation from '@shared/components/kakaoMap/hooks/useGetLocation';
@@ -6,22 +5,49 @@ import type { LatLng } from '@shared/components/kakaoMap/types/latLng';
 import { MAP_SIZE, type MapSize } from '@shared/components/kakaoMap/constant/mapSize';
 import * as styles from '@shared/components/kakaoMap/KakaoMap.css';
 
+import type { PinData } from '@shared/components/kakaoMap/mockup';
+
 interface KakaoMapProps {
-  center: LatLng | null;
+  center?: LatLng;
   size?: MapSize;
+  pinData?: PinData[];
 }
 
-const KakaoMap = ({ center, size = MAP_SIZE.MEDIUM }: KakaoMapProps) => {
+const KakaoMap = ({ center, size = MAP_SIZE.MEDIUM, pinData }: KakaoMapProps) => {
   const loaded = useKakaoLoader();
-  const { position } = useGetLocation();
+  const { currentCenter, position } = useGetLocation(center);
 
-  const fallbackCenter = useMemo(() => center ?? position, [center, position]);
+  const fallbackCenter = center ?? currentCenter;
 
   if (!loaded) return <div>지도 불러오는 중...</div>;
 
   return (
     <Map center={fallbackCenter} className={styles.mapWrapper({ size })}>
-      <MapMarker position={fallbackCenter} />
+      {/* 검색된 장소가 있으면 검색된 장소에, 없으면 현재 위치에 마커 */}
+      <MapMarker
+        position={center || position}
+        image={{
+          src: '/icon/ic_location_dot.svg',
+          size: { width: 50, height: 50 },
+        }}
+      />
+
+      {/* pinData가 있으면 해당 위치들에 마커 표시 */}
+      {pinData &&
+        pinData.map((pin, index) => (
+          <MapMarker
+            key={`pin-${pin.pinId}-${index}`}
+            position={{
+              lat: pin.latitude,
+              lng: pin.longitude,
+            }}
+            image={{
+              // TODO: 핀 컴포넌트로 변경 필요
+              src: '/icon/ic_location_dot.svg',
+              size: { width: 35, height: 35 },
+            }}
+          />
+        ))}
     </Map>
   );
 };
