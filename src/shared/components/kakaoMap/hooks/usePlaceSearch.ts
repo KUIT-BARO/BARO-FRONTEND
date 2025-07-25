6528f2e97e24fe70d5603fda4fcbc073d3ad96c9
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-25 12:34:17 +0900

    refactor: 검색 결과 전부 return

--- src/shared/components/kakaoMap/hooks/usePlaceSearch.ts
+++ src/shared/components/kakaoMap/hooks/usePlaceSearch.ts
@@ -1,8 +1,7 @@
-import { useCallback } from 'react';
 import type { LatLng } from '@shared/components/kakaoMap/types/latLng';
 
 function usePlaceSearch() {
-  const searchPlace = useCallback((keyword: string): Promise<LatLng> => {
+  const searchPlace = (placeName: string): Promise<LatLng[]> => {
     return new Promise((resolve, reject) => {
       if (!window.kakao || !window.kakao.maps) {
         return reject(new Error('Kakao Maps is not loaded'));
@@ -10,16 +9,20 @@ function usePlaceSearch() {
 
       const ps = new window.kakao.maps.services.Places();
 
-      ps.keywordSearch(keyword, (result, status) => {
+      ps.keywordSearch(placeName, (result, status) => {
         if (status === window.kakao.maps.services.Status.OK) {
-          const { x, y } = result[0];
-          resolve({ lng: parseFloat(x), lat: parseFloat(y) });
+          console.log(result);
+          const places = result.map((place: any) => ({
+            lng: parseFloat(place.x),
+            lat: parseFloat(place.y),
+          }));
+          resolve(places);
         } else {
           reject(new Error('Place not found'));
         }
       });
     });
-  }, []);
+  };
 
   return { searchPlace };
 }
