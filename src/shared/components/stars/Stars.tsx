6eda41798c186b176f6679355892dc8f5d5c34ad
hwanggyuun <gka365@naver.com>
2025-07-27 15:47:45 +0900

    feat: stars 컴포넌트 구현

---
+++ src/shared/components/stars/Stars.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import * as styles from '@shared/components/stars/Stars.css'
+import { SelectStars } from './SelectStars';
+
+interface StarsProps {
+    score: number;
+    size: 'small' | 'big' | 'default';
+}
+
+export const Stars: React.FC<StarsProps> = ({ score, size }) => {
+  const clampedScore = Math.max(0, Math.min(5, score));
+  const { Fill, None , Half } = SelectStars({ size });
+  return (
+    <div className={styles.container}>
+      {Array.from({ length: 5 }, (_, index) => {
+        if (index < Math.floor(clampedScore)) {
+          return <span key={index}>{Fill}</span>;
+        } else if (index < Math.ceil(clampedScore)) {
+          return <span key={index}>{Half}</span>;
+        } else {
+          return <span key={index}>{None}</span>;
+        }
+      })}
+    </div>
+  )
+}
