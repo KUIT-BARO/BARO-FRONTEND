105cee4ca1985971f28ddc393091dcfe6f455e1d
hwanggyuun <gka365@naver.com>
2025-07-28 02:50:17 +0900

    feat: Calendar function들 빼놓기

---
+++ src/shared/components/calendar/DateArray.ts
@@ -0,0 +1,69 @@
+import {
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  startOfWeek,
+  endOfWeek,
+  isAfter,
+  isWithinInterval,
+} from 'date-fns';
+
+interface DateArrayProps {
+  date: Date;
+}
+
+export interface DateArrayAnswer {
+  day: number;
+  date: Date;
+  isCurrentMonth: boolean;
+}
+
+export function DateArray({ date }: DateArrayProps): DateArrayAnswer[][] {
+  const startMonth = startOfMonth(date);
+  const endMonth = endOfMonth(date);
+  const startDate = startOfWeek(startMonth);
+  const endDate = endOfWeek(endMonth);
+  const currentMonth = date.getMonth();
+
+  const daysInMonth = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  });
+  const days: DateArrayAnswer[] = daysInMonth.map(d => ({
+    day: d.getDate(),
+    date: d,
+    isCurrentMonth: d.getMonth() === currentMonth,
+  }));
+  const weeks: DateArrayAnswer[][] = [];
+  for (let i = 0; i < days.length; i += 7) {
+    weeks.push(days.slice(i, i + 7));
+  }
+
+  return weeks;
+}
+
+export function isInRange(day: Date, start: Date, end: Date) {
+  const [from, to] = isAfter(start, end) ? [end, start] : [start, end];
+  return isWithinInterval(day, { start: from, end: to });
+}
+
+export function getDayColor(
+  day: DateArrayAnswer,
+  today: Date,
+  selectedDate: Date,
+  dragStart: Date | null,
+  dragEnd: Date | null
+) {
+  const dayDateString = day.date.toDateString();
+  if (dayDateString === today.toDateString() || dayDateString === selectedDate.toDateString()) {
+    return 'white';
+  }
+  if (dragStart && dragEnd && isInRange(day.date, dragStart, dragEnd)) {
+    return 'white';
+  }
+  if (day.isCurrentMonth) {
+    return 'isMonth';
+  }
+
+  return 'notMonth';
+}
