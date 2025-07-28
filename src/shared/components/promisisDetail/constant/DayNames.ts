74048d400ebf79e561d1ace1ec23f00c21941467
hwanggyuun <gka365@naver.com>
2025-07-28 15:50:20 +0900

    add: 상수 파일 추가

---
+++ src/shared/components/promisisDetail/constant/DayNames.ts
@@ -0,0 +1,7 @@
+export const DAY_NAMES = ['일', '월', '화', '수', '목', '금', '토'];
+
+export const PROMISIS_STATUS_LABEL: Record<string, string> = {
+  suggestedPromises: '미정',
+  votingPromises: '투표',
+  confirmedPromises: '확정',
+};
