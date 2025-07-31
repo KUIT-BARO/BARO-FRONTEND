941fd79275483e3d91f071885f21944e26758eae
hwanggyuun <gka365@naver.com>
2025-07-31 19:25:02 +0900

    fix: 이전 날짜 클릭 방지 및 이전 다음 달 버튼 리팩토링

--- src/shared/components/calendar/Calendar.tsx
+++ src/shared/components/calendar/Calendar.tsx
@@ -5,26 +5,30 @@ import clsx from 'clsx';
 import { IcArrowBlueLeft, IcArrowBlueRight } from '@svg/index';
 import { DateArray, getDayColor, isInRange } from '@shared/components/calendar/utils/DateArray';
 import type { DateArrayAnswer } from '@shared/components/calendar/types/Calendar.type';
-import { format, addMonths, subMonths } from 'date-fns';
+import { format, addMonths, subMonths, isBefore, startOfDay } from 'date-fns';
 import { week } from '@shared/components/kakaoMap/constant/week';
 
 export default function Calendar() {
   const todayRef = useRef(new Date());
   const today = todayRef.current;
+  const [baseDate, setBaseDate] = useState<Date>(today);
   const [clickStart, setClickStart] = useState<Date>(new Date());
   const [clickEnd, setClickEnd] = useState<Date | null>(null);
   const [isOneClicked, setIsOneClicked] = useState(false);
-  const selectedMonthYear = format(clickStart, 'yyyy MMM');
-  const dayArray: DateArrayAnswer[][] = DateArray({ date: clickStart });
+  const selectedMonthYear = format(baseDate, 'yyyy MMM');
+  const dayArray: DateArrayAnswer[][] = DateArray({ date: baseDate });
 
   const onPrevMonth = () => {
-    setClickStart(subMonths(clickStart, 1));
+    setBaseDate(subMonths(baseDate, 1));
   };
   const onNextMonth = () => {
-    setClickStart(addMonths(clickStart, 1));
+    setBaseDate(addMonths(baseDate, 1));
   };
 
   const handleClick = (date: Date) => {
+    if (isBefore(date, startOfDay(today))) {
+      return;
+    }
     if (!isOneClicked) {
       setClickStart(date);
       setIsOneClicked(true);
@@ -62,13 +66,11 @@ export default function Calendar() {
                 key={dayStr}
                 className={styles.dateItem({
                   isToday: dayStr === today.toDateString() ? 'True' : 'default',
-                  isSelected: dayStr === clickStart.toDateString() ? 'True' : 'default',
-                  isInRange: clickStart && clickEnd && isInRange(day.date, clickStart, clickEnd) ? 'True' : 'default',
+                  isInRange: isInRange(day.date, clickStart, clickEnd) ? 'True' : 'default',
                 })}
                 onClick={() => handleClick(day.date)}
-
               >
-                <Text tag='calendartext' color={getDayColor(day, today, clickStart, clickStart, clickEnd)}  className={clsx(styles.dayText)} >
+                <Text tag='calendartext' color={getDayColor(day, today, clickStart, clickEnd)}  className={clsx(styles.dayText)} >
                   {day.day}
                 </Text>
               </div>
