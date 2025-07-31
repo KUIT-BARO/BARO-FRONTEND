2444973fb74df2f073499f9c66438431a215cdf4
hwanggyuun <gka365@naver.com>
2025-07-31 18:52:40 +0900

    fix: drag 기능 click으로 대체 구현

--- src/shared/components/calendar/Calendar.stories.tsx
+++ src/shared/components/calendar/Calendar.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import {Calendar} from '@shared/components/calendar/Calendar';
+import  Calendar  from '@shared/components/calendar/Calendar';
 
 const meta: Meta<typeof Calendar> = {
   title: 'Components/Calendar',
