76c90d749c5566b863e23412bce173ff351dbd39
hwanggyuun <gka365@naver.com>
2025-08-05 02:08:08 +0900

    feat: blue arrow icons index 파일에 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -62,3 +62,5 @@ export { default as IcPersonBlack } from '@svg/ic_person_black.svg?react';
 export { default as IcClockBlack } from '@svg/ic_clock_black.svg?react';
 export { default as IcArrowRight } from '@svg/ic_arrow_right.svg?react';
 export { default as IcScope } from '@svg/ic_scope.svg?react';
+export { default as IcArrowBlueLeft } from '@svg/ic_arrow_blue_left.svg?react';
+export { default as IcArrowBlueRight } from '@svg/ic_arrow_blue_right.svg?react';
