a66e40f80f6fa293127b5d35f5806623ad89a163
junghyungo <pp90971658@gmail.com>
2025-08-10 01:27:01 +0900

    docs: storybook 코드 삭제

--- src/pages/home/components/homeAddPromise/HomeAddPromise.stories.tsx
+++
@@ -1,8 +0,0 @@
-import HomeAddPromise from '@pages/home/components/homeAddPromise/HomeAddPromise';
-
-export default {
-  title: 'Home/HomeAddPromise',
-  component: HomeAddPromise,
-};
-
-export const Default = () => <HomeAddPromise />;
