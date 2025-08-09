f06b3f0624554d7cbabd0650bac98be577900a84
hwanggyuun <gka365@naver.com>
2025-08-09 15:10:09 +0900

    fix: zod 수정

--- src/pages/logIn/hook/useLogInForm.ts
+++ src/pages/logIn/hook/useLogInForm.ts
@@ -3,15 +3,15 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const loginSchema = z.object({
-  email: z.string().email('이메일 형식이 올바르지 않습니다.'),
+  email: z.string().min(1, '이메일을 입력해주세요').email('이메일 형식이 올바르지 않습니다.'),
   password: z
     .string()
+    .min(8, { message: '비밀번호는 8자 이상이어야 합니다.' })
+    .max(20, { message: '비밀번호는 20자 이하여야 합니다.' })
     .regex(
       /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,20}$/,
       '문자와 특수문자, 숫자가 혼합된 8~20자리의 비밀번호를 입력해주세요.'
-    )
-    .min(8, { message: '비밀번호는 8자 이상이어야 합니다.' })
-    .max(20, { message: '비밀번호는 20자 이하여야 합니다.' }),
+    ),
 });
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
