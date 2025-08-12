eb4bf399d53f56f057b72069ee6e9a0e76fbf047
hwanggyuun <gka365@naver.com>
2025-08-13 00:47:03 +0900

    fix: error 메세지 및 zod 수정

--- src/pages/logIn/hook/useLogInForm.ts
+++ src/pages/logIn/hook/useLogInForm.ts
@@ -3,15 +3,15 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const loginSchema = z.object({
-  email: z.string().min(1, '이메일을 입력해주세요').email('이메일 형식이 올바르지 않습니다.'),
+  email: z.string().nonempty('이메일을 입력해주세요').email('이메일 형식이 올바르지 않습니다.'),
   password: z
     .string()
     .min(8, { message: '비밀번호는 8자 이상이어야 합니다.' })
     .max(20, { message: '비밀번호는 20자 이하여야 합니다.' })
-    .regex(
-      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,20}$/,
-      '문자와 특수문자, 숫자가 혼합된 8~20자리의 비밀번호를 입력해주세요.'
-    ),
+    .refine(val => val === '' || /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,20}$/.test(val), {
+      message: '문자와 특수문자, 숫자가 혼합된 8~20자리의 비밀번호를 입력해주세요.',
+    })
+    .nonempty({ message: '비밀번호를 입력해주세요.' }),
 });
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
@@ -23,33 +23,32 @@ export function useLogInForm() {
       password: '',
     },
     resolver: zodResolver(loginSchema),
-    mode: 'onSubmit',
+    mode: 'onChange',
   });
 
   const emailValue = watch('email');
   const passwordValue = watch('password');
   const { errors, isValid } = formState;
-
   const onSubmit = (data: LoginFormValues) => {
     console.log('Form submitted with data:', data);
   };
+
   const onSubmitError = () => {
     if (errors.email) {
       setFocus('email');
-      alert(errors.email.message);
     } else if (errors.password) {
       setFocus('password');
-      alert(errors.password.message);
     }
   };
+
   return {
     register,
     handleSubmit,
-    watch,
     emailValue,
     passwordValue,
     onSubmit,
     onSubmitError,
     isValid,
+    errors,
   };
 }
