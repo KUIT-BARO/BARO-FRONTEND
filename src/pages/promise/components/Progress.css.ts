bf86c33e953e8ab57c75287f3e0790b710fea680
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-08-18 16:34:36 +0900

    style: css 수정

--- src/pages/promise/components/Progress.css.ts
+++ src/pages/promise/components/Progress.css.ts
@@ -6,10 +6,12 @@ export const progressWrapper = style({
   height: '0.6rem',
   background: vars.color.gray1,
   marginBottom: '1.6rem',
+  borderRadius: '2.4rem',
 });
 
 export const progressBar = style({
   width: '100%',
   height: '100%',
   background: vars.color.baroBlue,
+  borderRadius: '2.4rem',
 });
--- src/pages/promise/step/Step.css.ts
+++ src/pages/promise/step/Step.css.ts
@@ -6,6 +6,7 @@ export const stepWrapper = style({
   width: '100%',
   display: 'flex',
   flexDirection: 'column',
+  padding: ' 0 1.6rem',
 });
 
 export const textWrapper = style({
