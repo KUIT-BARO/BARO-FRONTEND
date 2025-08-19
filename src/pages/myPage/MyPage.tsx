583a2581177c083996ba407c00320224e0abe1a3
hwanggyuun <gka365@naver.com>
2025-08-19 20:34:28 +0900

    [feat] 시간표 제외 마이페이지 구현

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -1,3 +1,20 @@
+import ProfileHeader from "@pages/setting/component/ProfileHeader";
+import Header from "@shared/components/header/Header";
+import { IcSetting, IcPlusBlack, IcShareBlack } from "@svg/index";
+import Text from "@shared/components/text/Text";
+import * as styles from "@pages/myPage/MyPage.css";
+
 export default function MyPage() {
-  return <div></div>;
+  return <div className={styles.container}>
+    <Header text="마이페이지" rightIcon={IcSetting} />
+    <ProfileHeader isSetting = {true}/>
+    <div className={styles.scheduleTitleBox}>
+      <Text tag="body_bold_19">일정표</Text>
+      <div className={styles.optionBox}>
+        <IcPlusBlack className={styles.optionSvg} />
+        <IcShareBlack className={styles.optionSvg} />
+      </div>
+    </div>
+    {/* 여기에 일정표 컴포넌트 추가 예정 */}
+  </div>;
 }
