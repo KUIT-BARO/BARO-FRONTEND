10f7e8283c76191fd3d8caa13f3c9fde7cca1e0f
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-08-20 15:42:21 +0900

    fix: lint error

--- src/shared/components/promiseDetail/utils/getStatusLines.tsx
+++ src/shared/components/promiseDetail/utils/getStatusLines.tsx
@@ -8,25 +8,25 @@ export default function getStatusLines(props: PromiseDetailProps): {
   thirdLine: string;
 } {
   switch (props.status) {
-    case PROMISE_STATUS.PENDING:
-      return {
-        firstLine: `투표까지 D-${props.untilVoteDate > 0 ? props.untilVoteDate : 'DAY'}`,
-        secondLine: props.suggestedRegion,
-        thirdLine: `${formatDateWithDay(props.suggestedStartDate)} ~ ${formatDateWithDay(props.suggestedEndDate)}`,
-      };
-    case PROMISE_STATUS.VOTING:
-      return {
-        firstLine: `${props.untilVoteEndDate}일`,
-        secondLine: props.suggestedRegion,
-        thirdLine: `${formatDateWithDay(props.suggestedStartDate)} ~ ${formatDateWithDay(props.suggestedEndDate)}`,
-      };
-    case PROMISE_STATUS.CONFIRMED:
-      return {
-        firstLine: props.promiseMembersNames.join(', '),
-        secondLine: props.placeName,
-        thirdLine: formatDateWithDay(props.fixedDate),
-      };
-    default:
-      throw new Error(`Unknown promise status`);
+  case PROMISE_STATUS.PENDING:
+    return {
+      firstLine: `투표까지 D-${props.untilVoteDate > 0 ? props.untilVoteDate : 'DAY'}`,
+      secondLine: props.suggestedRegion,
+      thirdLine: `${formatDateWithDay(props.suggestedStartDate)} ~ ${formatDateWithDay(props.suggestedEndDate)}`,
+    };
+  case PROMISE_STATUS.VOTING:
+    return {
+      firstLine: `${props.untilVoteEndDate}일`,
+      secondLine: props.suggestedRegion,
+      thirdLine: `${formatDateWithDay(props.suggestedStartDate)} ~ ${formatDateWithDay(props.suggestedEndDate)}`,
+    };
+  case PROMISE_STATUS.CONFIRMED:
+    return {
+      firstLine: props.promiseMembersNames.join(', '),
+      secondLine: props.placeName,
+      thirdLine: formatDateWithDay(props.fixedDate),
+    };
+  default:
+    throw new Error(`Unknown promise status`);
   }
 }
