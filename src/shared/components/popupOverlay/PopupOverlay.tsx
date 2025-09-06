b9936712f006a620dab3949292b1b2a11345a3dc
hwanggyuun <gka365@naver.com>
2025-09-07 00:29:29 +0900

    fix: rfc

--- src/shared/components/popupOverlay/PopupOverlay.tsx
+++ src/shared/components/popupOverlay/PopupOverlay.tsx
@@ -12,25 +12,22 @@ interface PopupOverlayProps {
   children: React.ReactNode;
 }
 
-const PopupOverlay: React.FC<PopupOverlayProps> =
-  ({ children,onClose,open ,position,top=false,toptitle}) => {
-    if (!open) {
-      return null;
-    }
-    return (
-      <div className={styles.container} onClick={onClose}>
-        <div className={styles.dim} />
-        {top && <div className={styles.top}>
-          <IcArrowLeft className={styles.closeIcon}/>
-          <Text tag="body_bold_19" color="white"
-            className={styles.title}>{toptitle}</Text>
-          <div className={styles.empty}/>
-        </div>}
-        <div className={styles.content({position})}
-          onClick={(e) => e.stopPropagation()}
-        >{children}</div>
-      </div>
-    );
-  };
-
-export default PopupOverlay;
+export default function PopupOverlay({ children, onClose, open, position, top = false, toptitle }: PopupOverlayProps) {
+  if (!open) {
+    return null;
+  }
+  return (
+    <div className={styles.container} onClick={onClose}>
+      <div className={styles.dim} />
+      {top && <div className={styles.top}>
+        <IcArrowLeft className={styles.closeIcon}/>
+        <Text tag="body_bold_19" color="white"
+          className={styles.title}>{toptitle}</Text>
+        <div className={styles.empty}/>
+      </div>}
+      <div className={styles.content({position})}
+        onClick={(e) => e.stopPropagation()}
+      >{children}</div>
+    </div>
+  );
+};
