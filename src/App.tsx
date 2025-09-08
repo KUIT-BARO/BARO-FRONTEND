d27f15ed996b5f0baef04fe1fd2ba348a5712ed8
hwanggyuun <gka365@naver.com>
2025-09-08 20:15:56 +0900

    fix: QueryClient 인스턴스 중복 생성 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import router from '@router/Router';
 import '@styles/global.css.ts';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from '@api/queryClient';
 
 const App = () => {
-  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
